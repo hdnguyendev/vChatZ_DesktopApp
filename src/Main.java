e8568ae4433fe7aad021655ea0137ec75e04a3ef
hdnguyendev <nguyenhd.dev@gmail.com>
2023-05-29 19:09:57 +0700

    🚀feature: create-app

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+/*
+# Project Name: vChatZ
+# Description: App chat (multi-platform) socket.io with nodejs server
+# Author: Hồ Đăng Nguyện (hdnguyendev)
+# Task:
+[] build client ui using java swing (desktop app)
+[] build client ui using java/kotlin (android app)
+[] build client ui using vue js (web)
+[] build server with node js + socket io
+[] completed app java desktop, build file .exe
+[] completed app android, build file .apk
+[] completed web, deploy up to web
+*/
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
